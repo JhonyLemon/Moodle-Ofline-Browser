b8f05339789812898b542b071adf98d5ce54c85e
bartek <bartek_urb@wp.pl>
2022-04-06 14:40:23 +0200

    fghfgh

--- Moodle Ofline Browser GUI/ViewModels/EnterBackupFileViewModel.cs
+++ Moodle Ofline Browser GUI/ViewModels/EnterBackupFileViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +11,21 @@ namespace Moodle_Ofline_Browser_GUI.ViewModels
     class EnterBackupFileViewModel : Screen
     {
         private IEventAggregator _eventAggregator;
+        private String path;
 
         public EnterBackupFileViewModel(IEventAggregator eventAggregator)
         {
             _eventAggregator = eventAggregator;
+        }
+
+        public void ChooseFile()
+        {
+            var dialog = new OpenFileDialog();
+
+            if(dialog.ShowDialog() == true)
+            {
+                path = dialog.FileName;
+            }
         }
     }
 }
